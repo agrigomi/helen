9ac2d4cf593bf3698594ae5d56cd5b852a6ab061
Mihail Grigorov <agrigomi@abv.bg>
2023-03-17 10:51:55 +0200

    http: Command line options.
    Signed-off-by: Mihail Grigorov <agrigomi@abv.bg>

--- http/src/http.h
+++ http/src/http.h
@@ -1,7 +1,16 @@
 #ifndef __HTTP_H__
 #define __HTTP_H__
 
+// command line options
+#define OPT_HELP	"help"
+#define OPT_SHELP	"h"
+#define OPT_VER		"v"
+#define OPT_DIR		"dir"
 
+// HTTP request variables
+#define REQ_METHOD	"METHOD"
+#define REQ_URL		"URL"
+#define REQ_PROTOCOL	"PROTOCOL"
 
 #endif
 
--- http/src/main.cpp
+++ http/src/main.cpp
@@ -1,7 +1,40 @@
+#include <stdio.h>
 #include "config.h"
 #include "http.h"
+#include "argv.h"
+
+static _argv_t args[] = {
+	{ OPT_HELP,	OF_LONG,			NULL,		"Print this help" },
+	{ OPT_SHELP,	0,				NULL,		"Print this help" },
+	{ OPT_VER,	0,				NULL,		"Print version" },
+	{ OPT_DIR,	OF_LONG | OF_VALUE,		NULL,		"Settings directory" },
+	//...
+	{ NULL,		0,				NULL,		NULL }
+};
+
+static void usage(void) {
+	int n = 0;
+
+	printf("options:\n");
+	while(args[n].opt_name) {
+		if(args[n].opt_flags & OF_LONG)
+			printf("--%s:      \t%s\n", args[n].opt_name, args[n].opt_help);
+		else
+			printf("-%s:      \t%s\n", args[n].opt_name, args[n].opt_help);
+
+		n++;
+	}
+	printf("Usage: http [options]\n");
+}
 
 int main(int argc, char *argv[]) {
+	if(argc > 1 && argv_parse(argc, (_cstr_t *)argv, args)) {
+		if(argv_check(OPT_SHELP) || argv_check(OPT_HELP))
+			usage();
+		if(argv_check(OPT_VER))
+			printf("%s\n", VERSION);
+	} else
+		usage();
 
 	return 0;
 }
