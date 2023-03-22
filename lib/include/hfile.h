ab3aaba5303d9753ef938a249b97ab2561fe35c9
Mihail Grigorov <agrigomi@abv.bg>
2023-03-22 21:23:08 +0200

    lib: hfile.h
    Signed-off-by: Mihail Grigorov <agrigomi@abv.bg>

--- lib/include/hfile.h
+++ lib/include/hfile.h
@@ -4,6 +4,7 @@
 #include "sha1.h"
 
 #define HF_INVALID_OFFSET	0xffffffffffffffff
+#define HF_MAX_PATH		512
 
 /* File header */
 typedef struct __attribute__((packed)) {
@@ -22,14 +23,54 @@ typedef struct __attribute__((packed)) {
 
 /* hash file context */
 typedef struct {
+	char		path[HF_MAX_PATH];	/* Path to file */
+	int		flags;		/* file access flags */
 	int		fd;		/* file descriptor */
 	unsigned long	size;		/* total file size */
 	_hf_hdr_t 	*hdr;		/* file content (mapping) */
 	unsigned long	*htable;	/* pointer to first element in hash table */
 	_hf_rec_hdr_t 	*first;		/* pointer to first data record */
-	_hf_rec_hdr_t	*next;		/* pointer to next record */
 } _hf_context_t;
 
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+/**
+Create hash file */
+int hf_create(_hf_context_t *, const char *path, unsigned int capacity);
+
+/**
+Open hash file.
+path	- path to file
+flags	- O_RDONLY or O_RDWR ... see open (man 2 open)
+returns 0 for success or -1 for fail */
+int hf_open(_hf_context_t *, const char *path, int flags);
+
+/**
+Close hash file */
+void hf_close(_hf_context_t *);
+
+/**
+Add record to hash file.
+key	- record identifier
+sz_key	- size of record identifier
+data	- pointer to record content
+sz_data	- size of record content
+returns pointer to record content in case of success, or NULL */
+void *hf_add(_hf_context_t *, void *key, int sz_key, void *data, int sz_data);
+
+/**
+Search for record in hash file
+key	- record identifier
+sz_key	- size of record identifier
+returns pointer to record or NULL  */
+void *hf_get(_hf_context_t *, void *key, int sz_key);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+
+#endif /* __HFILE_H__ */
 
