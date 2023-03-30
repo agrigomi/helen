ee5169cdcf3dd2f8f6e3191a9e4a147ba040f5dd
Mihail Grigorov <m.grigorov@printecgroup.com>
2023-03-30 14:45:39 +0300

    http: Setup SSL context.
    Signed-off-by: Mihail Grigorov <m.grigorov@printecgroup.com>

--- http/src/http.h
+++ http/src/http.h
@@ -1,6 +1,8 @@
 #ifndef __HTTP_H__
 #define __HTTP_H__
 
+#include "api_ssl.h"
+
 // command line options
 #define OPT_HELP	"help"
 #define OPT_SHELP	"h"
@@ -10,10 +12,15 @@
 #define OPT_SSL_KEY	"ssl-key"
 #define OPT_SSL_METHOD	"ssl-method"
 
+
 // HTTP request variables
 #define REQ_METHOD	"METHOD"
 #define REQ_URL		"URL"
 #define REQ_PROTOCOL	"PROTOCOL"
+
+extern SSL_CTX	*_g_ssl_context_;
+extern SSL	*_g_ssl_in_;
+extern SSL	*_g_ssl_out_;
 
 #endif
 
--- http/src/main.cpp
+++ http/src/main.cpp
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 #include "config.h"
 #include "http.h"
 #include "argv.h"
+#include "trace.h"
 
 static _argv_t args[] = {
 	{ OPT_HELP,		OF_LONG,				NULL,				"Print this help" },
@@ -10,10 +14,14 @@ static _argv_t args[] = {
 	{ OPT_DIR,		OF_LONG | OF_VALUE | OF_PRESENT,	(_str_t)"./.config/helen",	"Settings directory" },
 	{ OPT_SSL_CERT,		OF_LONG | OF_VALUE,			NULL,				"SSL certificate file (PEM only)" },
 	{ OPT_SSL_KEY,		OF_LONG | OF_VALUE,			NULL,				"SSL private key file (PEM only)" },
-	{ OPT_SSL_METHOD,	OF_LONG | OF_VALUE | OF_PRESENT,	(_str_t)"TLSv1_2",		"SSL server method (SSLv23, TLSv1_2, DTLS, TLS)" },
+	{ OPT_SSL_METHOD,	OF_LONG | OF_VALUE | OF_PRESENT,	NULL,				"SSL server method (SSLv23, TLSv1_2, DTLS, TLS)" },
 	//...
 	{ NULL,			0,					NULL,				NULL }
 };
+
+SSL_CTX *_g_ssl_context_ = NULL;
+SSL	*_g_ssl_in_ = NULL;
+SSL	*_g_ssl_out_ = NULL;
 
 static void usage(void) {
 	int n = 0;
@@ -31,17 +39,63 @@ static void usage(void) {
 	printf("Usage: http [options]\n");
 }
 
+static void setup_ssl_context(const char *method, const char *cert, const char *key) {
+	const SSL_METHOD *ssl_method = ssl_select_method(method);
+
+	if (ssl_method) {
+		if ((_g_ssl_context_ = ssl_create_context(ssl_method))) {
+			if (ssl_load_cert(_g_ssl_context_, cert)) {
+				if (ssl_load_key(_g_ssl_context_, key)) {
+					_g_ssl_in_ = SSL_new(_g_ssl_context_);
+
+					SSL_set_fd(_g_ssl_in_, STDIN_FILENO);
+					SSL_set_accept_state(_g_ssl_in_);
+
+					/* Because of one descriptor for stdin and stdout */
+					_g_ssl_out_ = _g_ssl_in_;
+				} else {
+					TRACE("http[%d] Failed to load SSL key '%s'\n", getpid(), key);
+					SSL_CTX_free(_g_ssl_context_);
+					_g_ssl_context_ = NULL;
+				}
+			} else {
+				TRACE("http[%d] Failed to load SSL certificate '%s'\n", getpid(), cert);
+				SSL_CTX_free(_g_ssl_context_);
+				_g_ssl_context_ = NULL;
+			}
+		} else {
+			TRACE("http[%d] Unable to allocate SSL context\n", getpid());
+		}
+	} else {
+		TRACE("http[%d] Unsupported SSL method '%s'\n", getpid(), method);
+	}
+}
+
 int main(int argc, char *argv[]) {
-	if (argc > 1 && argv_parse(argc, (_cstr_t *)argv, args)) {
+	const char *ssl_method = NULL;
+	const char *ssl_cert = NULL;
+	const char *ssl_key = NULL;
+
+	if (argv_parse(argc, (_cstr_t *)argv, args)) {
 		if (argv_check(OPT_SHELP) || argv_check(OPT_HELP))
 			usage();
 		if (argv_check(OPT_VER))
 			printf("%s\n", VERSION);
 
-		if (argv_check(OPT_SSL_CERT) && argv_check(OPT_SSL_KEY)) {
+		if (!(ssl_method = argv_value(OPT_SSL_METHOD)))
+			ssl_method = getenv(OPT_SSL_METHOD);
+		if (!(ssl_cert = argv_value(OPT_SSL_CERT)))
+			ssl_cert = getenv(OPT_SSL_CERT);
+		if (!(ssl_key = argv_value(OPT_SSL_KEY)))
+			ssl_key = getenv(OPT_SSL_KEY);
+
+		if (ssl_method && ssl_cert && ssl_key) {
 			// setup SSL context
-			// ...
+			TRACE("http[%d] Setup SSL method='%s'; cert='%s'; key='%s'\n", getpid(), ssl_method, ssl_cert, ssl_key);
+			setup_ssl_context(ssl_method, ssl_cert, ssl_key);
 		}
+
+		//...
 	} else
 		usage();
 
