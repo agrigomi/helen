9931aed9ca868323d7a683412bfb761c2cf38b4b
Mihail Grigorov <m.grigorov@printecgroup.com>
2023-03-30 17:19:15 +0300

    http: WIP
    Signed-off-by: Mihail Grigorov <m.grigorov@printecgroup.com>

--- http/src/main.cpp
+++ http/src/main.cpp
@@ -2,10 +2,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sys/wait.h>
 #include "config.h"
 #include "http.h"
 #include "argv.h"
 #include "trace.h"
+#include "sig.h"
 
 static _argv_t args[] = {
 	{ OPT_HELP,		OF_LONG,				NULL,				"Print this help" },
@@ -51,8 +54,16 @@ static void setup_ssl_context(const char *method, const char *cert, const char *
 					SSL_set_fd(_g_ssl_in_, STDIN_FILENO);
 					SSL_set_accept_state(_g_ssl_in_);
 
-					/* Because of one descriptor for stdin and stdout */
-					_g_ssl_out_ = _g_ssl_in_;
+					if (SSL_accept(_g_ssl_in_) == 1)
+						/* Because of one descriptor for stdin and stdout */
+						_g_ssl_out_ = _g_ssl_in_;
+					else {
+						TRACE("http[%d] SSL handshake failed\n", getpid());
+						SSL_CTX_free(_g_ssl_context_);
+						SSL_free(_g_ssl_in_);
+						_g_ssl_context_ = NULL;
+						_g_ssl_in_ = NULL;
+					}
 				} else {
 					TRACE("http[%d] Failed to load SSL key '%s'\n", getpid(), key);
 					SSL_CTX_free(_g_ssl_context_);
@@ -72,9 +83,41 @@ static void setup_ssl_context(const char *method, const char *cert, const char *
 }
 
 int main(int argc, char *argv[]) {
+	int r = 0;
 	const char *ssl_method = NULL;
 	const char *ssl_cert = NULL;
 	const char *ssl_key = NULL;
+
+	signal(SIGCHLD, [](__attribute__((unused)) int sig) {
+		int stat;
+		pid_t	pid;
+
+		while (1) {
+			if ((pid = wait3 (&stat, WNOHANG, (struct rusage *)NULL )) <= 0)
+				break;
+			TRACE("http[%d]: SIGCHLD: PID=%u, STATUS=%d\n", getpid(), pid, stat);
+		}
+	});
+	signal(SIGSEGV, [](__attribute__((unused)) int sig) {
+		TRACE("http: SIGSEGV\n");
+		dump_stack();
+		exit(0);
+	});
+	signal(SIGINT, [](__attribute__((unused)) int sig) {
+		TRACE("http[%d]: SIGINT\n", getpid());
+		exit(0);
+	});
+	signal(SIGKILL, [](__attribute__((unused)) int sig) {
+		TRACE("http[%d]: SIGKILL\n", getpid());
+		exit(0);
+	});
+	signal(SIGTERM, [](__attribute__((unused)) int sig) {
+		TRACE("http[%d]: SIGTERM\n", getpid());
+		exit(0);
+	});
+	signal(SIGPIPE, [](__attribute__((unused)) int sig) {
+		TRACE("http[%d]: SIGPIPE\n", getpid());
+	});
 
 	if (argv_parse(argc, (_cstr_t *)argv, args)) {
 		if (argv_check(OPT_SHELP) || argv_check(OPT_HELP))
@@ -96,8 +139,15 @@ int main(int argc, char *argv[]) {
 		}
 
 		//...
-	} else
-		usage();
 
-	return 0;
+		if (_g_ssl_in_)
+			SSL_free(_g_ssl_in_);
+		if (_g_ssl_context_)
+			SSL_CTX_free(_g_ssl_context_);
+	} else {
+		usage();
+		r = -1;
+	}
+
+	return r;
 }
